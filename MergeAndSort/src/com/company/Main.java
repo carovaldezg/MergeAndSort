b4c9f58d504df8ad9a5f90285aa73b4e7d5c61f7
carovaldezg <caro.valdezg@gmail.com>
2016-12-21 00:09:27 -0300

    second commit

--- MergeAndSort/src/com/company/Main.java
+++ MergeAndSort/src/com/company/Main.java
@@ -1,3 +1,6 @@
+//Developed by Carolina VG on Dec 21st, 2016.
+
+
 package com.company;
 
 public class Main {
@@ -41,39 +44,98 @@ public class Main {
 
             while (i < max_a && j < max_b) {
 
-                if (src_a[i] <= src_b[j]) {
+                //caso en el que el elemento "i" del arreglo A es menor que el elemento j del arreglo B
+                if (src_a[i] < src_b[j]) {
                     result[k] = src_a[i];
                     i++;
                     k++;
                 }
-                else {
+                //caso en el que el elemento "j" del arreglo B es menor que el elemento i del arreglo A
+                else
+                    if (src_b[j] < src_a[i]) {
                         result[k] = src_b[j];
                         j++;
                         k++;
                      }
-             }
+                    //Caso en el que los elementos de ambos arreglos son iguales.
+                     else
 
+                          if (k > 0)
+                              //Si en el resultado ya hay un valor repetido, no se debe volver a agregar
+                              if (result[k-1] != src_a[i]) {
+                                result[k] = src_a[i];
+                                i++;
+                                j++;
+                                k++;
+                                }
+                                else
+                                    i++;
+                          //Este caso se utiliza cuando los arreglos tienen un solo elemento y este tiene el mismo valor numerico.
+                          //Entonces, no tengo ningun elemento en el arreglo resultado todavia y solo almaceno uno de los valores.
+                           else
+                            {
+                                result[k] = src_a[i];
+                                k++;
+                                i++;
+                            }
+
+
+
+            }
+
+        //Si el arreglo A es mas largo que el B agrego todos sus valores a resultado si estos no estan repetidos
         while (i < max_a) {
-            result[k] = src_a[i];
+
+            if (k > 0) {
+                if (src_a[i] != result[k-1]) {
+                    result[k] = src_a[i];
+                    k++;
+                }
+
+            }
+            //Caso en el que hay un solo elemento y el arreglo resultado no posee elementos todavia.
+            else
+            {
+                result[k] = src_a[i];
+                k++;
+            }
             i++;
-            k++;
+
         }
 
+        //Lo mismo sucede si el arreglo B es mas largo que el A
         while (j < max_b) {
-            result[k] = src_b[j];
+           if (k > 0) {
+               if (src_b[j] != result[k - 1]) {
+                   result[k] = src_b[j];
+
+                   k++;
+               }
+
+           }
+           else
+               result[k] = src_b[j];
             j++;
-            k++;
         }
 
-
+        printArray (result);
     }
 
 
+
+
     public static void main(String[] args) {
-        int [] src_a = {};
-        int [] src_b = {};
+
+        int [] src_a = {8,10,10};
+        int [] src_b = {1,2,9};
         int [] result = new int [src_a.length + src_b.length];
+
+        //Ya que no se admiten elementos repetidos, es necesario iniciar las posiciones del arreglo resultado con algún valor característico
+        //debido a que la longitud continúa siendo la sumas de las longitudes de los arreglos A y B. Esto permite una mejor comprensión de los
+        //datos del arreglo que se muestran por consola al finalizar la ejecución del método merge.
+        for (int index = 0; index < result.length; index++)
+            result[index] = -1000;
         merge(src_a, src_b, result);
-        printArray (result);
+
     }
 }
